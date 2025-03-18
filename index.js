68bfcd5343db6de803942ecdaf2f7fcb4da82fc3
Sir Harunzy <Harunzy55@gmail.com>
2025-03-18 23:01:57 +0530

    add js content

---
+++ index.js
@@ -0,0 +1,40 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElements = document.getElementById("question");
+const answerButtonsElements = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+let quizScore = 0;
+
+const questions = [
+    {
+        question : "Which of this is a javascript framework?",
+        answer : [
+            { text: "Python", correct: false },
+            { text: "Django", correct: false },
+            { text: "React", correct: true },
+            { text: "Eclipse", correct: false }
+        ],
+    },
+    {
+        question : "Who is the Education minister in Ghana?",
+        answer : [
+            { text: "Hamza", correct: false },
+            { text: "Haruna", correct: true },
+            { text: "Muaz", correct: false }
+        ],
+    },
+    {
+        question : "What is 4*4?",
+        answer : [
+            { text: "8", correct: false },
+            { text: "10", correct: false },
+            { text: "16", correct: true },
+        ],
+    },
+]
+
+
+
+
